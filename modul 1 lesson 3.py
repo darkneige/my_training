98098c0a10c2e07f647702423b86ed84ad88a16c
darkneige <sanyasmilecool@gmail.com>
2024-11-03 19:29:02 +0700

    Add files via upload

---
+++ modul 1 lesson 3.py
@@ -0,0 +1,14 @@
+example = 'darkneige'
+print(example[0])
+
+#exercise 3
+print(example[-1])
+
+#exercise 4
+print(example[len(example)//2:])
+
+#exercise 5
+print(example[::-1])
+
+#exercise 6
+print(example[1::2])

2864e5c3b212f34eb4a7088a9a055d9f56d05650
darkneige <sanyasmilecool@gmail.com>
2024-11-11 21:49:01 +0700

    Модуль 2 урок 2.py

--- Модуль 2 урок 2.py
+++ Модуль 2 урок 2.py
@@ -1,9 +1,9 @@
 first = int(input('Введите первое число: '))
 second = int(input('Введите второе число: '))
 third = int(input('Введите третье число: '))
-if first==second==third :
+if first == second == third :
     print('3')
-elif first==second or first==third or second==third:
+elif first == second or first == third or second == third :
     print('2')
 else:
     print('0')

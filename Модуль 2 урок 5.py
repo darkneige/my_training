869159478ba8f6bccd6fbaa477de1a57f3808595
darkneige <sanyasmilecool@gmail.com>
2024-11-16 22:55:37 +0700

    Add files via upload

---
+++ Модуль 2 урок 5.py
@@ -0,0 +1,15 @@
+def get_matrix(n, m, value):
+    matrix = []
+    for i in range(n):
+        matrix.append([])
+        for j in range(m):
+            matrix[i].append(value)
+    return matrix
+
+n = int(input('Количество строк матрицы :'))
+m = int(input('Количество столбцов матрицы :'))
+value = input(f'Значение : ')
+matrix = get_matrix(n, m, value)
+print("Матрица воплоти:")
+for i in matrix:
+    print(*i)
